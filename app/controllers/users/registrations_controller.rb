d1c2aee5534fc6c39b2669512beb047d8fabf24e
Marek Swiderek <maronq1212@gmail.com>
2025-01-16 17:45:44 +0100

    Added User Serializer, configured registrations_controller and sessions_controller Devise JSON communication.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,6 +39,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  private
+
+  def respond_with(resource, _opts = {})
+    if request.method == "POST" && resource.persisted?
+      render json: {
+        status: { code: 200, message: "Signed up succesfully." },
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }, status: :ok
+    elsif request.method == "DELETE"
+      render json: {
+        status: { code: 200, message: "User deleted succesfully." }
+      }
+    else
+      render json: {
+        status: { code: 422, message: "User can't be created: #{resource.errors.full_error_messages.to_sentence}" }
+      }, status: :unprocessable_entity
+    end
+  end
+
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
