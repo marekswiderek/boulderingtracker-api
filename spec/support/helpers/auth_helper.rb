15c486b96f34f9a2ff10e7e5319398417dab4cd3
Marek Swiderek <maronq1212@gmail.com>
2025-01-21 16:29:07 +0100

    Removed unused variable from TestHelpers::AuthHelper.

--- spec/support/helpers/auth_helper.rb
+++ spec/support/helpers/auth_helper.rb
@@ -4,7 +4,7 @@ module TestHelpers
   module AuthHelper
     def provide_auth_headers(user)
       headers = { 'Accept' => 'application/json', 'Content-type' => 'application/json' }
-      auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, user)
+      Devise::JWT::TestHelpers.auth_headers(headers, user)
     end
   end
 end
